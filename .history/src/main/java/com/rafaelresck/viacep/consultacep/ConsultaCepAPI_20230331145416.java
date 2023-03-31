1ebcf8406d980aa8f96f703665e7b4c17142d705
rrresck <rrresck@gmail.com>
2023-03-31 14:56:30 -0300

    ulitmo commit

---
+++ .history/src/main/java/com/rafaelresck/viacep/consultacep/ConsultaCepAPI_20230331145416.java
@@ -0,0 +1,29 @@
+package com.rafaelresck.viacep.consultacep;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("consulta-cep")
+public class ConsultaCepAPI {
+    @GetMapping("{cep}")
+    public CepResultDTO consultCep(@PathVariable("cep") String cep) {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<CepResultDTO> resp = restTemplate.getForEntity(
+                String.format("https://viacep.com.br/ws/%s/json/", cep), CepResultDTO.class);
+        CepResultDTO cepResultDTO = resp.getBody();
+        if (cepResultDTO == null) {
+            throw new RuntimeException("CEP não encontrado");
+        }
+        double frete = cepResultDTO.calcularFrete();
+        cepResultDTO.setFrete(String.format("%.2f", frete));
+        cepResultDTO.setLocalidade(cepResultDTO.getLocalidade());
+        cepResultDTO.setLogradouro(cepResultDTO.getLogradouro());
+        return cepResultDTO;
+    }
+    
+}

3a52da8428ba032c19d2dda2f8cf24f3e5daff9f
rrresck <rrresck@gmail.com>
2023-03-31 10:58:26 -0300

    conclusão código

--- .history/src/main/java/com/rafaelresck/viacep/consultacep/ConsultaCepAPI_20230331104540.java
+++ .history/src/main/java/com/rafaelresck/viacep/consultacep/ConsultaCepAPI_20230331104540.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("consulta-cep")
@@ -13,17 +15,15 @@ public class ConsultaCepAPI {
     @GetMapping("{cep}")
     public CepResultDTO consultCep(@PathVariable("cep") String cep) {
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<CepResultDTO> resp = restTemplate.getForEntity(
-                String.format("https://viacep.com.br/ws/%s/json/", cep), CepResultDTO.class);
+        ResponseEntity<CepResultDTO> resp = respTemplate.getForEntity(
+            String.format(format:"https://viacep.com.br/ws/%s/json/", cep), responseType:CepResultDTO.class);
         CepResultDTO cepResultDTO = resp.getBody();
         if (cepResultDTO == null) {
-            throw new RuntimeException("CEP não encontrado");
+            throw new RuntimeException(message:"CEP não encontrado");
         }
-        double frete = cepResultDTO.calcularFrete();
-        cepResultDTO.setFrete(String.format("%.2f", frete));
-        cepResultDTO.setLocalidade(cepResultDTO.getLocalidade());
-        cepResultDTO.setLogradouro(cepResultDTO.getLogradouro());
-        return cepResultDTO;
+
+        
+        return new cepResultDTO;
     }
     
 }
--- .history/src/main/java/com/rafaelresck/viacep/consultacep/ConsultaCepAPI_20230331105156.java
+++ .history/src/main/java/com/rafaelresck/viacep/consultacep/ConsultaCepAPI_20230331105156.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("consulta-cep")
@@ -13,7 +15,7 @@ public class ConsultaCepAPI {
     @GetMapping("{cep}")
     public CepResultDTO consultCep(@PathVariable("cep") String cep) {
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<CepResultDTO> resp = restTemplate.getForEntity(
+        ResponseEntity<CepResultDTO> resp = respTemplate.getForEntity(
                 String.format("https://viacep.com.br/ws/%s/json/", cep), CepResultDTO.class);
         CepResultDTO cepResultDTO = resp.getBody();
         if (cepResultDTO == null) {

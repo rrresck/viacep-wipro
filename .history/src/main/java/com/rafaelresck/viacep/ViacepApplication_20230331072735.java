6bf0b105ccdbd64ebbbcce9a2d5faa03c17a74db
rrresck <rrresck@gmail.com>
2023-03-31 05:02:13 -0300

    ajustes frete

--- .history/src/main/java/com/rafaelresck/viacep/ViacepApplication_20230331072735.java
+++ .history/src/main/java/com/rafaelresck/viacep/ViacepApplication_20230331072735.java
@@ -2,9 +2,7 @@ package com.rafaelresck.viacep;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger2
 @SpringBootApplication
 public class ViacepApplication {
 
